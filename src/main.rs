dd0d84e4201aff1bc271495aa1e4da3144442711
Hugo Laloge <hla@lescompanions.com>
2023-06-08 16:11:51 +0200

    feat: allow to specify a regular file to read APRS reports

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
+use flightlogr::ogn::OGN_APRS_URL;
 use std::fs::File;
 use std::io::Read;
 use std::net::TcpStream;
-use flightlogr::ogn::OGN_APRS_URL;
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
-use serde::{Deserialize};
-use flightlogr::aprs::client::{APRSClient, Credentials, Filter, FilterSpec};
+use flightlogr::aprs::{
+    client::{APRSClient, Credentials, Filter, FilterSpec, Reports},
+    Report,
+};
+use serde::Deserialize;
 
 /// Merged CLI arguments and config.
 #[derive(Clone, Debug, Deserialize, Parser)]
 #[command(author, version, about, long_about = None)]
 struct PartialConfig {
     #[arg(long, help=format!("APRS URL [default: {}]", OGN_APRS_URL))]
-    aprs_url: Option<String>,
+    aprs_uri: Option<String>,
     #[arg(long)]
     aprs_user: Option<String>,
     #[arg(long)]
     aprs_password: Option<String>,
-    #[arg(short, long, help="Config file path")]
+    #[arg(short, long, help = "Config file path")]
     #[serde(skip)]
     config_file: Option<String>,
     #[clap(flatten)]
@@ -38,8 +41,8 @@ struct FilterConfig {
 impl PartialConfig {
     /// Replace this config missing fields with other's.
     pub fn merge_missing(&mut self, other: PartialConfig) {
-        if self.aprs_url.is_none() {
-            self.aprs_url = other.aprs_url;
+        if self.aprs_uri.is_none() {
+            self.aprs_uri = other.aprs_uri;
         }
         if self.aprs_user.is_none() {
             self.aprs_user = other.aprs_user;
@@ -54,30 +57,37 @@ impl PartialConfig {
 
     pub fn complete(self) -> Result<Config> {
         let PartialConfig {
-            aprs_url,
+            aprs_uri,
             aprs_user,
             aprs_password,
             filters,
             ..
         } = self;
         Ok(Config {
-            aprs_url: aprs_url.unwrap_or_else(|| OGN_APRS_URL.to_owned()),
-            aprs_user: aprs_user.ok_or_else(|| anyhow!("missing APRS username"))?,
-            aprs_password: aprs_password.ok_or_else(|| anyhow!("missing APRS password"))?,
-            filters: vec![Filter::new(FilterSpec::Range {
-                lat: filters.as_ref().ok_or_else(|| anyhow!("missing latitude"))?.latitude.ok_or_else(|| anyhow!("missing latitude"))?,
-                lon: filters.as_ref().ok_or_else(|| anyhow!("missing latitude"))?.longitude.ok_or_else(|| anyhow!("missing longitude"))?,
-                range: filters.as_ref().ok_or_else(|| anyhow!("missing latitude"))?.range.ok_or_else(|| anyhow!("missing range"))?,
-            })]
+            aprs_uri: aprs_uri.unwrap_or_else(|| OGN_APRS_URL.to_owned()),
+            aprs_user: aprs_user,
+            aprs_password: aprs_password,
+            filters: match filters.as_ref() {
+                Some(FilterConfig {
+                    latitude: Some(latitude),
+                    longitude: Some(longitude),
+                    range: Some(range),
+                }) => vec![Filter::new(FilterSpec::Range {
+                    lat: *latitude,
+                    lon: *longitude,
+                    range: *range,
+                })],
+                _ => vec![],
+            },
         })
     }
 }
 
 #[derive(Clone, Debug)]
 struct Config {
-    aprs_url: String,
-    aprs_user: String,
-    aprs_password: String,
+    aprs_uri: String,
+    aprs_user: Option<String>,
+    aprs_password: Option<String>,
     filters: Vec<Filter>,
 }
 
@@ -107,17 +117,8 @@ impl Config {
 
 fn main() -> Result<()> {
     let config = Config::parse();
-    let stream = TcpStream::connect(config.aprs_url)?;
-    let client = APRSClient::login(stream, &Credentials {
-        user: config.aprs_user.to_string(),
-        password: config.aprs_password.to_string(),
-        app_name: "flightLGo".to_owned(), // env!("CARGO_PKG_NAME").to_owned(),
-        app_version: "0.0.0b1".to_string(),//, env!("CARGO_PKG_VERSION").to_owned(),
-    },
-                                   &config.filters,
-                                   false)?;
-
-    for report in client.reports() {
+    let report_stream = open_reports(config)?;
+    for report in report_stream {
         match report {
             Ok(r) => println!("{}", r),
             Err(e) => eprintln!("could not parse report: {}", e),
@@ -125,4 +126,44 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn open_reports(config: Config) -> Result<Box<dyn Iterator<Item = Result<Report>>>> {
+    match config.aprs_uri.as_str() {
+        "-" => Ok(Box::new(Reports::new(std::io::stdin()))),
+        file_uri if file_uri.starts_with("file://") => {
+            let path = &file_uri[7..];
+            let stream = File::open(path)?;
+            Ok(Box::new(Reports::new(stream)))
+        }
+        file_uri if file_uri.starts_with("./") => {
+            let path = &file_uri[2..];
+            let stream = File::open(path)?;
+            Ok(Box::new(Reports::new(stream)))
+        }
+        url => {
+            if config.filters.is_empty() {
+                eprintln!("WARNING: No filters declared.");
+            }
+            let stream = TcpStream::connect(url)?;
+            let client = APRSClient::login(
+                stream,
+                &Credentials {
+                    user: config
+                        .aprs_user
+                        .ok_or_else(|| anyhow!("missing APRS username"))?
+                        .to_string(),
+                    password: config
+                        .aprs_password
+                        .ok_or_else(|| anyhow!("missing APRS password"))?
+                        .to_string(),
+                    app_name: "flightLGo".to_owned(), // env!("CARGO_PKG_NAME").to_owned(),
+                    app_version: "0.0.0b1".to_string(), //, env!("CARGO_PKG_VERSION").to_owned(),
+                },
+                &config.filters,
+                false,
+            )?;
+            Ok(Box::new(client.reports()))
+        }
+    }
 }
